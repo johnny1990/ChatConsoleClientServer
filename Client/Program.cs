369e0a306cabced4c037bed1d370c8c40a24f25d
Man Ioan <man_ioan@live.com>
2021-05-26 18:00:28 +0300

    Implement functionalities for chat(client&server)

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 namespace Client
 {
     class Program
     {
+        static IPAddress ip;
+        static Socket socket;
+        static Thread thread;
+        static string chatUserName;
+
+        static string GetLocalIp()
+        {
+            string hostName = Dns.GetHostName();
+            IPHostEntry entry = Dns.GetHostEntry(hostName);
+            IPAddress[] address = entry.AddressList;
+            return address[address.Length - 1].ToString();
+        }
+
+        static void Receive()
+        {
+            while (true)
+            {
+                Thread.Sleep(50);
+                byte[] buffer = new byte[300];
+                int receive = socket.Receive(buffer, 0, buffer.Length, 0);
+                Array.Resize(ref buffer, receive);
+                Console.WriteLine(Encoding.Default.GetString(buffer));
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            thread = new Thread(Receive);
+            Console.WriteLine("------------------Chat Client------------------------");
+            Console.WriteLine("Please enter your name and press enter");
+
+
+            chatUserName = Console.ReadLine();
+            ip = IPAddress.Parse(GetLocalIp());
+            int port = 1234;
+
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(new IPEndPoint(ip, port));
+            thread.Start();
+
+            byte[] data = Encoding.Default.GetBytes("<" + chatUserName + "> Connected to the chat");
+            socket.Send(data, 0, data.Length, 0);
+
+            while (socket.Connected)
+            {  
+                byte[] SendDataToServer = Encoding.Default.GetBytes("<" + chatUserName + ">" + Console.ReadLine());
+                socket.Send(SendDataToServer, 0, SendDataToServer.Length, 0);
+            }
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 namespace Server
 {
     class Program
     {
+        static IPAddress ip;
+        static Thread thread;
+        static Socket socket;
+        static Socket acceptSocket;
+        static string serverChatUserName;
+
+        static string GetLocalIp()
+        {
+            string hostName = Dns.GetHostName();
+            IPHostEntry entry = Dns.GetHostEntry(hostName);
+            IPAddress[] address = entry.AddressList;
+            return address[address.Length - 1].ToString();
+        }
+
+
+        static void Receive()
+        {
+            while (true)
+            {
+                Thread.Sleep(50);
+                byte[] buffer = new byte[300];
+                int receive = acceptSocket.Receive(buffer, 0, buffer.Length, 0);
+                Array.Resize(ref buffer, receive);
+                Console.WriteLine(Encoding.Default.GetString(buffer));
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            thread = new Thread(Receive);
+            Console.WriteLine("--------------Chat Server----------------------------");
+            Console.WriteLine("Please enter your name and press enter");
+            serverChatUserName = Console.ReadLine();
+
+            Console.WriteLine("<" + serverChatUserName + "> Connected to the chat");
+            Console.WriteLine("Please enter the name in client console app to connect and have a chat!");
+
+            int port = 1234;
+
+            ip = IPAddress.Parse(GetLocalIp());
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(ip, port));
+            socket.Listen(0);
+            acceptSocket = socket.Accept();
+            thread.Start();
+
+            while (true)
+            {   
+                byte[] SendDataToClient = Encoding.Default.GetBytes("<" + serverChatUserName + ">" + Console.ReadLine());
+                acceptSocket.Send(SendDataToClient, 0, SendDataToClient.Length, 0);
+            }
         }
     }
 }
